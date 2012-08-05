964f54e520d70699fa69265079efe4f1460b8a73
AlejoNext <fotocopia@galeph.com>
2012-08-04 22:07:48 -0500

    M

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -9,47 +9,47 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:require) do |s|
 		s.trigger = 'req'
-		s.expansion = 'var $1 = require(\'${1:sys}\')'
+		s.expansion = 'var $1 = require(\'${1:sys}\');$0'
 	end
 
 	snippet t(:addListener) do |s|
 		s.trigger = 'addL'
 		s.expansion = 'addListener(\'${1:event}\', function(${2:}){
 	${3:}
-});'
+});$0'
 	end
 
 	snippet t(:app) do |s|
 		s.trigger = 'app'
 		s.expansion = 'app.${1:method}(\'${2:path}\', function(req, res, next){
 	${3:}
-});'
+});$0'
 	end
 
 	snippet t(:env) do |s|
 		s.trigger = '.env'
-		s.expansion = 'process.env.${1:VARIABLE}'
+		s.expansion = 'process.env.${1:VARIABLE}$0'
 	end
 
 	snippet t(:filter) do |s|
 		s.trigger = 'fil'
 		s.expansion = 'filter(function(item){
 	return ${1:false};
-});'
+});$0'
 	end
 
 	snippet t(:forEach) do |s|
 		s.trigger = 'forE'
 		s.expansion = 'forEach(function(${1:item},${2:index}){
 	${3:}
-});'
+});$0'
 	end
 
 	snippet t(:on) do |s|
 		s.trigger = '.on'
 		s.expansion = '.on(\'${1:eventname}\', function(${2:err, res}){
 	${3:}
-});'
+});$0'
 	end
 
 	snippet t(:render) do |s|
@@ -58,45 +58,45 @@ with_defaults :scope => 'source.js' do
 	locals:{
 		${2:templatevars}
 	}
-});'
+});$0'
 	end
 
 	snippet t(:setInterval) do |s|
 		s.trigger = 'setI'
-		s.expansion = 'setInterval(${1:callback}, ${2:delay})'
+		s.expansion = 'setInterval(${1:callback}, ${2:delay})$0'
 	end
 
 	snippet t(:console) do |s|
 		s.trigger = 'con'
-		s.expansion = 'console.${1:sintax}(\'${2:message}\');'
+		s.expansion = 'console.${1:sintax}(\'${2:message}\');$0'
 	end
 
 	snippet 'JSON.stringify' do |s|
 		s.trigger = 'JSON.s'
-		s.expansion = 'JSON.stringify(${1:{some:\'object\'}});'
+		s.expansion = 'JSON.stringify(${1:{some:\'object\'}});$0'
 	end
 
 	snippet 'JSON.parse' do |s|
 		s.trigger = 'JSON.p'
-		s.expansion = 'JSON.parse(${1:\'{\'string\':\'of json\'}});'
+		s.expansion = 'JSON.parse(${1:\'{\'string\':\'of json\'}});$0'
 	end
 
 	snippet t(:readFile) do |s|
 		s.trigger = 'fs.r'
-		s.expansion = 'fs.readFile(\'${1:filename}\', function(err, data){
-	${2: }
-});'
+		s.expansion = 'fs.readFile(\'${1:filename}\', function(err, ${2:Data}){
+	${2:Data}
+});$0'
 	end
 
 	snippet t(:modelfind) do |s|
 		s.trigger = 'modelfind'
 		s.expansion = '${1:Model}.find({ ${2:} }).all(function(${3:results}){
-	${4:}
-});'
+	${3:results}
+});$0'
 	end
 
 	snippet t(:exports) do |s|
 		s.trigger = 'exp'
-		s.expansion = 'exports.${1:Model} = '
+		s.expansion = 'exports.${1:Model} = ${2:Var/function(){ \n\t };/};$0'
 	end
 end
