acd11ec788ba6a55c4c4488e9165ea24463574bd
AlejoNext <fotocopia@galeph.com>
2012-08-04 22:17:53 -0500

    s.scope

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -4,16 +4,19 @@ with_defaults :scope => 'source.js' do
 
 	snippet 'dirname' do |s|
 		s.trigger = 'dir'
+		s.scope = 'source.js'
 		s.expansion = '__dirname'
 	end
 
 	snippet t(:require) do |s|
 		s.trigger = 'req'
+		s.scope = 'source.js'
 		s.expansion = 'var $1 = require(\'${1:sys}\');$0'
 	end
 
 	snippet t(:addListener) do |s|
 		s.trigger = 'addL'
+		s.scope = 'source.js'
 		s.expansion = 'addListener(\'${1:event}\', function(${2:Var}){
 	${2:Var}
 });$0'
@@ -21,6 +24,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:app) do |s|
 		s.trigger = 'app'
+		s.scope = 'source.js'
 		s.expansion = 'app.${1:method}(\'${2:path}\', function(req, res, next){
 	
 });$0'
@@ -28,11 +32,13 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:env) do |s|
 		s.trigger = '.env'
+		s.scope = 'source.js'
 		s.expansion = 'process.env.${1:VARIABLE}$0'
 	end
 
 	snippet t(:filter) do |s|
 		s.trigger = 'fil'
+		s.scope = 'source.js'
 		s.expansion = 'filter(function(item){
 	return ${1:false};
 });$0'
@@ -40,6 +46,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:forEach) do |s|
 		s.trigger = 'forE'
+		s.scope = 'source.js'
 		s.expansion = 'forEach(function(${1:item},${2:index}){
 	
 });$0'
@@ -47,6 +54,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:on) do |s|
 		s.trigger = '.on'
+		s.scope = 'source.js'
 		s.expansion = '.on(\'${1:eventname}\', function(${2:err, res}){
 	
 });$0'
@@ -54,6 +62,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:render) do |s|
 		s.trigger = '.ren'
+		s.scope = 'source.js'
 		s.expansion = '.render(\'${1:template}\', {
 	locals:{
 		${2:template Vars}
@@ -63,26 +72,31 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:setInterval) do |s|
 		s.trigger = 'setI'
+		s.scope = 'source.js'
 		s.expansion = 'setInterval(${1:callback}, ${2:delay});$0'
 	end
 
 	snippet t(:console) do |s|
 		s.trigger = 'con'
+		s.scope = 'source.js'
 		s.expansion = 'console.${1:sintax}(\'${2:message}\');$0'
 	end
 
 	snippet 'JSON.stringify' do |s|
 		s.trigger = 'JSON.s'
+		s.scope = 'source.js'
 		s.expansion = 'JSON.stringify(${1:{some:\'object\'}});$0'
 	end
 
 	snippet 'JSON.parse' do |s|
 		s.trigger = 'JSON.p'
+		s.scope = 'source.js'
 		s.expansion = 'JSON.parse(${1:\'{\'string\':\'of json\'}});$0'
 	end
 
 	snippet t(:readFile) do |s|
 		s.trigger = 'fs.r'
+		s.scope = 'source.js'
 		s.expansion = 'fs.readFile(\'${1:filename}\', function(err, ${2:data}){
 	${2:data}
 });$0'
@@ -90,6 +104,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:modelfind) do |s|
 		s.trigger = 'modelfind'
+		s.scope = 'source.js'
 		s.expansion = '${1:Model}.find({ ${2:} }).all( function(${3:results}){
 	${3:results}
 });$0'
@@ -97,6 +112,7 @@ with_defaults :scope => 'source.js' do
 
 	snippet t(:exports) do |s|
 		s.trigger = 'exp'
+		s.scope = 'source.js'
 		s.expansion = 'exports.${1:Model} = ${2:Var/function(){ \n\t };/};$0'
 	end
 end
